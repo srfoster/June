5344c6010cebbb8fc95b311d186a070a65451edf
Stephen Foster <stephenfoster@f0-b4-79-1d-44-d7.dynamic.ucsd.edu>
2011-12-13 10:27:04 -0800

    Updating the version of YouGitIt that's bundled with June

--- Assets/Editor/YouGitIt.js
+++ Assets/Editor/YouGitIt.js
@@ -1,3 +1,4 @@
+/* See the copyright information at https://github.com/srfoster/YouGitIt/blob/master/COPYRIGHT */
 
 /*
 TODO: Make it actually fetch the correct file.
@@ -22,11 +23,6 @@ class YouGitIt extends EditorWindow {
     }
 
     function OnGUI () {
-        if(!Directory.Exists(data_path+"/YouGotIt"))
-		{
-            Directory.CreateDirectory(data_path+"/YouGotIt");
-        }
-        
 		if(GUI.Button(Rect(Screen.width/2 - 50,Screen.height/2-30,100,20),button_text) && button_text != "Gitting...")
 		{
 			button_text = "Gitting...";
@@ -51,21 +47,29 @@ class YouGitIt extends EditorWindow {
 		    	
 		    	for(var to_git in to_gits)
 		    	{
-					var to_git_path_split = to_git.Split('/'[0]);
-					var package_name = to_git_path_split[to_git_path_split.Length - 1];
-					
-			    	Debug.Log("Gitting " + to_git + "/blob/master/"+package_name+".unitypackage?raw=true");
-					feedback = "Gitting: " + to_git;
-		    		
-		    		var webClient = new WebClient();
-					webClient.DownloadFile(to_git + "/blob/master/"+package_name+".unitypackage?raw=true", data_path + "/YouGotIt/"+package_name+".unitypackage");
+		    		if(to_git != "")
+		    		{
+		    			try{
+							var to_git_path_split = to_git.Split('/'[0]);
+							var package_name = to_git_path_split[to_git_path_split.Length - 1];
+							
+					    	Debug.Log("Gitting " + to_git);
+							feedback = "Gitting: " + to_git;
+				    		
+				    		var webClient = new WebClient();
+							webClient.DownloadFile(to_git, data_path + "/Vendor/YouGitIt/"+package_name);
+						}catch(e){
+							Debug.Log(e);
+							feedback = e.ToString();
+						}
+					}
 		    	}
 		    	
 		    	
 		    }
 		
 			button_text = "Git It!";
-			feedback = "Got It!  Refresh your YouGotIt folder.";
+			feedback = "Got It!  Refresh your Vendor/YouGitIt/ folder";
 		}catch(e){
 			feedback = e.ToString();
 		}
